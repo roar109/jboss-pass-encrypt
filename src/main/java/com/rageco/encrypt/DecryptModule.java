9ef189965ec79898a85235245ed1b4546d120267
hector.mendoza <roar109@gmail.com>
2014-12-08 15:21:17 -0600

    Add decrypt class and dependencies

--- src/main/java/com/rageco/encrypt/DecryptModule.java
+++ src/main/java/com/rageco/encrypt/DecryptModule.java
@@ -1,9 +1,10 @@
 package com.rageco.encrypt;
 
-import java.math.BigInteger;
+
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
+import java.math.BigInteger;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -11,54 +12,75 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 
+
 /**
- * @author hector.mendoza
- * 12/03/2014
+ * @author hector.mendoza 12/03/2014
  * */
-public class DecryptModule {
+public class DecryptModule
+{
 
-	public static void main(String[] args) {
-		try {
-			System.out.println(decode(args[0]));
-		} catch (Exception e) {
-			System.err.println(e.getMessage());
-			e.printStackTrace();
-		}
-	}
 
-	/**
-	 * Taken from <a href="https://source.jboss.org/browse/PicketBox/trunk/security-jboss-sx/jbosssx/src/main/java/org/picketbox/datasource/security/SecureIdentityLoginModule.java?r=276">Jboss api</a>
-	 * */
-	private static char[] decode(String secret) throws NoSuchPaddingException,
-			NoSuchAlgorithmException, InvalidKeyException, BadPaddingException,
-			IllegalBlockSizeException {
-		byte[] kbytes = "jaas is the way".getBytes();
-		SecretKeySpec key = new SecretKeySpec(kbytes, "Blowfish");
+   /**
+    * Represents main
+    *
+    * @param args
+    * @since 08/12/2014
+    *
+    */
+   public static void main (final String[] args)
+   {
+      try
+      {
+         // put here your string
+         System.out.println (decode (args[0]));
+      }
+      catch (final Exception e)
+      {
+         System.err.println (e.getMessage ());
+         e.printStackTrace ();
+      }
+   }
 
-		BigInteger n = new BigInteger(secret, 16);
-		byte[] encoding = n.toByteArray();
 
-		// SECURITY-344: fix leading zeros
-		if (encoding.length % 8 != 0) {
-			int length = encoding.length;
-			int newLength = ((length / 8) + 1) * 8;
-			int pad = newLength - length; // number of leading zeros
-			byte[] old = encoding;
-			encoding = new byte[newLength];
-			for (int i = old.length - 1; i >= 0; i--) {
-				encoding[i + pad] = old[i];
-			}
-			// SECURITY-563: handle negative numbers
-			if (n.signum() == -1) {
-				for (int i = 0; i < newLength - length; i++) {
-					encoding[i] = (byte) -1;
-				}
-			}
-		}
+   /**
+    * Taken from <a href=
+    * "https://source.jboss.org/browse/PicketBox/trunk/security-jboss-sx/jbosssx/src/main/java/org/picketbox/datasource/security/SecureIdentityLoginModule.java?r=276"
+    * >Jboss api</a>
+    * */
+   private static char[] decode (final String secret) throws NoSuchPaddingException, NoSuchAlgorithmException,
+         InvalidKeyException, BadPaddingException, IllegalBlockSizeException
+   {
+      final byte[] kbytes = "jaas is the way".getBytes ();
+      final SecretKeySpec key = new SecretKeySpec (kbytes, "Blowfish");
 
-		Cipher cipher = Cipher.getInstance("Blowfish");
-		cipher.init(Cipher.DECRYPT_MODE, key);
-		byte[] decode = cipher.doFinal(encoding);
-		return new String(decode).toCharArray();
-	}
+      final BigInteger n = new BigInteger (secret, 16);
+      byte[] encoding = n.toByteArray ();
+
+      // SECURITY-344: fix leading zeros
+      if ( (encoding.length % 8) != 0)
+      {
+         final int length = encoding.length;
+         final int newLength = ( (length / 8) + 1) * 8;
+         final int pad = newLength - length; // number of leading zeros
+         final byte[] old = encoding;
+         encoding = new byte[newLength];
+         for (int i = old.length - 1; i >= 0; i--)
+         {
+            encoding[i + pad] = old[i];
+         }
+         // SECURITY-563: handle negative numbers
+         if (n.signum () == -1)
+         {
+            for (int i = 0; i < (newLength - length); i++)
+            {
+               encoding[i] = (byte) -1;
+            }
+         }
+      }
+
+      final Cipher cipher = Cipher.getInstance ("Blowfish");
+      cipher.init (Cipher.DECRYPT_MODE, key);
+      final byte[] decode = cipher.doFinal (encoding);
+      return new String (decode).toCharArray ();
+   }
 }
