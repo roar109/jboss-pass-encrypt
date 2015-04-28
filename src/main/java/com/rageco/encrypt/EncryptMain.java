bb777609d27672b26e948b17006a50696364c4e3
Hector Mendoza <hmendoza@24hourfit.com>
2015-04-28 12:35:32 -0500

    Change main method parameters

--- src/main/java/com/rageco/encrypt/EncryptMain.java
+++ src/main/java/com/rageco/encrypt/EncryptMain.java
@@ -16,14 +16,16 @@ public class EncryptMain
     * Represents main
     *
     * @param args
+    *
     * @since 22/12/2014
     *
     */
-   public static void main (final String... args)
+   public static void main (final String[] args)
    {
       try
       {
-         org.picketbox.datasource.security.SecureIdentityLoginModule.main (new String[] {"somePass"});
+         // output is to the console
+         org.picketbox.datasource.security.SecureIdentityLoginModule.main (new String[] {args[0]});
       }
       catch (final Exception e)
       {
